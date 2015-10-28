eb4fe4559492550f9543bb8aa887a38360224e13
Paolo Tagliani <pablosproject@gmail.com>
2015-10-28 21:31:51 +0100

    Created test project

---
+++ PTToolkit/PTToolkit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PTToolkit
+//
+//  Created by Paolo Tagliani on 28/10/15.
+//  Copyright © 2015 Paolo Tagliani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PTToolkit/PTToolkit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PTToolkit
+//
+//  Created by Paolo Tagliani on 28/10/15.
+//  Copyright © 2015 Paolo Tagliani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
