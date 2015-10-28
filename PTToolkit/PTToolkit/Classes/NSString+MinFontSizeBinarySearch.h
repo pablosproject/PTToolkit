94399bd73bb0f4d082786cf107f9ac884d2c3bd7
Paolo Tagliani <pablosproject@gmail.com>
2015-10-28 21:47:52 +0100

    Changed copyright

--- PTToolkit/PTToolkit/Classes/NSString+MinFontSizeBinarySearch.h
+++ PTToolkit/PTToolkit/Classes/NSString+MinFontSizeBinarySearch.h
@@ -1,6 +1,6 @@
 //
 // Created by Paolo Tagliani on 21/10/15.
-// Copyright (c) 2015 Ideawise Ltd. All rights reserved.
+// Copyright (c) Paolo Tagliani. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
